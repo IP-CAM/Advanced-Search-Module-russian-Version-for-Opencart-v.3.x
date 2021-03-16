0fa8a7439e9d1f34b5a7b37867307d4d3c599dad
nik <nikandr33@inbox.ru>
2021-03-16 16:58:41 +0300

    on way

--- upload/catalog/controller/extension/module/advanced_search_nik.php
+++ upload/catalog/controller/extension/module/advanced_search_nik.php
@@ -8,8 +8,6 @@ class ControllerExtensionModuleAdvancedSearchNik extends Controller {
 
         $data = $this->model_setting_setting->getSetting('module_advanced_search_nik');
 
-//        var_dump($data);
-
         if ($data['module_advanced_search_nik_display_category']) {
             $data['categories'] = $this->model_extension_module_advanced_search_nik->getCategories();
         }
@@ -22,6 +20,7 @@ class ControllerExtensionModuleAdvancedSearchNik extends Controller {
 
         if (isset($this->request->get['filter_name'])) {
             $this->load->model('catalog/product');
+            $this->load->model('extension/module/advanced_search_nik');
             $this->load->model('setting/setting');
 
             $data = $this->model_setting_setting->getSetting('module_advanced_search_nik');
@@ -55,51 +54,57 @@ class ControllerExtensionModuleAdvancedSearchNik extends Controller {
                 'limit'              => $limit
             );
 
-            $results = $this->model_catalog_product->getProducts($filter_data);
+            $results = $this->model_extension_module_advanced_search_nik->getProducts($filter_data);
 
+            $products_categories = array();
             foreach ($results as $result) {
-                $option_data = array();
+                $product_categories = $this->model_extension_module_advanced_search_nik->getCategoriesByProduct($result['product_id']);
 
-                $product_options = $this->model_catalog_product->getProductOptions($result['product_id']);
-
-                foreach ($product_options as $product_option) {
-                    $option_info = $this->model_catalog_option->getOption($product_option['option_id']);
-
-                    if ($option_info) {
-                        $product_option_value_data = array();
-
-                        foreach ($product_option['product_option_value'] as $product_option_value) {
-                            $option_value_info = $this->model_catalog_option->getOptionValue($product_option_value['option_value_id']);
-
-                            if ($option_value_info) {
-                                $product_option_value_data[] = array(
-                                    'product_option_value_id' => $product_option_value['product_option_value_id'],
-                                    'option_value_id'         => $product_option_value['option_value_id'],
-                                    'name'                    => $option_value_info['name'],
-                                    'price'                   => (float)$product_option_value['price'] ? $this->currency->format($product_option_value['price'], $this->config->get('config_currency')) : false,
-                                    'price_prefix'            => $product_option_value['price_prefix']
-                                );
-                            }
-                        }
-
-                        $option_data[] = array(
-                            'product_option_id'    => $product_option['product_option_id'],
-                            'product_option_value' => $product_option_value_data,
-                            'option_id'            => $product_option['option_id'],
-                            'name'                 => $option_info['name'],
-                            'type'                 => $option_info['type'],
-                            'value'                => $product_option['value'],
-                            'required'             => $product_option['required']
-                        );
+                foreach ($product_categories as $product_category) {
+                    if (!in_array($product_category, $products_categories)) {
+                        $products_categories[] = $product_category;
                     }
                 }
+            }
 
+            $categories_info = array();
+
+            foreach ($products_categories as $product_category) {
+                $categories_info[] = $this->model_extension_module_advanced_search_nik->getCategory($product_category['category_id']);
+            }
+
+            foreach ($categories_info as $category_info) {
                 $json[] = array(
-                    'product_id' => $result['product_id'],
-                    'name'       => strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8')),
-                    'model'      => $result['model'],
-                    'option'     => $option_data,
-                    'price'      => $result['price']
+                    'product_id' => $category_info['category_id'],
+                    'name' => $category_info['name'],
+                    'type' => 'category'
+                );
+            }
+
+            $products_manufacturers = array();
+
+            foreach ($results as $result) {
+                $product_manufacturer= $this->model_extension_module_advanced_search_nik->getManufacturer($result['manufacturer_id']);
+                if(!in_array($product_manufacturer, $products_manufacturers)) {
+                    $products_manufacturers[] = $product_manufacturer;
+                }
+            }
+            
+            foreach ($products_manufacturers as $product_manufacturer) {
+                $json[] = array(
+                    'product_id' => $product_manufacturer['manufacturer_id'],
+                    'name' => $product_manufacturer['name'],
+                    'type' => 'manufacturer'
+                );
+            }
+
+            foreach ($results as $result) {
+                $json[] = array(
+                    'product_id'  => $result['product_id'],
+                    'name'        => strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8')),
+                    'model'       => $result['model'],
+                    'manufacturer'=> $result['manufacturer'],
+                    'price'       => $result['price'],
                 );
             }
         }
