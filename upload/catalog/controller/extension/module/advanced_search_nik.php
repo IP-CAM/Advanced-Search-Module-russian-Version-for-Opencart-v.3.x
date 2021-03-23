e4498aa2bd1c1fdf726a63babbc9bd2bd042450e
fruity nik <nikandr33@inbox.ru>
2021-03-23 10:23:29 +0300

    add link to category and brands

--- upload/catalog/controller/extension/module/advanced_search_nik.php
+++ upload/catalog/controller/extension/module/advanced_search_nik.php
@@ -110,11 +110,18 @@ class ControllerExtensionModuleAdvancedSearchNik extends Controller {
                 foreach ($categories_info as $category_info) {
                     $category_parents = $this->model_extension_module_advanced_search_nik->getCategoryParents($category_info['parent_id']);
 
+                    if (isset($category_info['parent_id']) && $category_info['parent_id'] != "0") {
+                        $link = $this->url->link('product/category', 'path=' . $category_info['parent_id'] . '_' . $category_info['category_id']);
+                    } else {
+                        $link = $this->url->link('product/category', 'path=' . $category_info['category_id']);
+                    }
+
                     $json[] = array(
 //                    'product_id' => $category_info['category_id'],
                         'name' => $category_info['name'],
                         'type' => 'category',
-                        'description' => $category_parents ? $category_parents['name'] : ''
+                        'description' => $category_parents ? $category_parents['name'] : '',
+                        'link' => $link
                     );
                 }
             }
@@ -138,7 +145,8 @@ class ControllerExtensionModuleAdvancedSearchNik extends Controller {
                         'name' => $product_manufacturer['name'],
                         'image' => $this->model_tool_image->resize($product_manufacturer['image'], 20, 20),
                         'type' => 'manufacturer',
-                        'description' => $this->language->get('text_brand')
+                        'description' => $this->language->get('text_brand'),
+                        'link' => $this->url->link('product/manufacturer/info', 'manufacturer_id=' . $product_manufacturer['manufacturer_id'])
                     );
                 }
             }
@@ -146,7 +154,8 @@ class ControllerExtensionModuleAdvancedSearchNik extends Controller {
             foreach ($results as $result) {
                 $json[] = array(
 //                    'product_id'  => $result['product_id'],
-                    'name'        => strip_tags(html_entity_decode($result['name'] . ' '. $result['manufacturer'], ENT_QUOTES, 'UTF-8')),
+                    'search_name' => strip_tags(html_entity_decode($result['name'] . ' '. $result['manufacturer'], ENT_QUOTES, 'UTF-8')),
+                    'name'        => strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8')),
                     'model'       => $result['model'],
                     'manufacturer'=> $result['manufacturer'],
                     'price'       => $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']),
